a91fa7aab6acfb5a0ea81321bf3de3fd0cf014d9
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-05-28 15:26:15 +0700

    menerapkan polymorphy di file solvemultiwaris dengan kata kunci virtual

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -11,7 +11,7 @@ public :
     }
 };
 
-class pekerja : public orang {
+class pekerja : virtual public orang {
 public:
     pekerja(int pUmur) : orang(pUmur)
     {
@@ -19,7 +19,7 @@ public:
     }
 };
 
-class pelajar : public orang {
+class pelajar : virtual public orang {
 public :
     pelajar(int pUmur) : orang(pUmur)
     {
@@ -29,7 +29,7 @@ public :
 
 class budi : public pekerja, public pelajar {
 public :
-    budi(int pUmur) : pekerja(pUmur), pelajar(pUmur)
+    budi(int pUmur) : pekerja(pUmur), pelajar(pUmur), orang(pUmur) //hal ini dapat dilakukan jika menggunakan virtual
     {
         cout << "Budi dibuat\n" << endl;
     }
